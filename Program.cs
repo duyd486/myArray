f0308485c3dfbd218f11060c4417cd2f3c16c3b6
duyd486 <duyd486@gmail.com>
2024-04-09 19:24:18 +0700

    add du an
    add du an

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿class Program{
+    public static void Main(string[] args)
+    {
+        int[] myArray = new int[10];
+        for(int i = 0; i < myArray.Length; i++) {
+            myArray[i] = i + 1;
+        } 
+        int sum = 0;
+        for(int i = 0; i < myArray.Length; i++) {
+            sum += myArray[i];
+        }
+        Console.WriteLine("Tong cua mang la " + sum);
+    }
+}
